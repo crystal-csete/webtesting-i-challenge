e7986a9ea9440f7e3c007e6959bd6160dcbd3a10
crystal-csete <crystal-csete@lambdastudents.com>
2021-01-27 16:25:00 -0700

    added tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,15 +5,47 @@ module.exports = {
   get,
 };
 
+// item: 'name', 'durability', 'enhancement'
+// enhancement num 0-20
+// durability num 0-100
+// name string: 'any'
+
+// enhancement SUCCESS:
+//item enhancement increases by 1
+//if num is 20, num is unchanged
+//also, durability is not changed
+
 function success(item) {
+  if (item.enhancement === 20) {
+    item.enhancement === 20;
+  } else {
+    item.enhancement = item.enhancement + 1;
+  }
   return { ...item };
 }
+
+// enhancement FAIL:
+//item < 15 durability = decreased by 5
+//item >= 15 durability = decreased by 10
+//item > 16 enhancement = decreased by 1
 
 function fail(item) {
+  if (item.enhancement < 15 && item.durability < 5) {
+    item.durability = 0;
+  } else if (item.enhancement < 15) {
+    item.durability = item.durability - 5;
+  } else if (item.enhancement === 15 || item.enhancement === 16) {
+    item.durability = item.durability - 10;
+  } else if (item.enhancement > 16) {
+    item.enhancement = item.enhancement - 1;
+  }
   return { ...item };
 }
 
+// durability num 0-100
+
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,92 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const enhancer = require("./enhancer.js");
+
+test("sanity", () => {
+  expect(3).toBe(3);
+  expect(3).not.toBe(4);
+  expect(3).toBeGreaterThan(2);
+});
+it("is not the same toEqual and toBe", () => {
+  let a = {};
+  let b = {};
+  let c = a;
+
+  expect(a).not.toBe(b);
+  expect(a).toEqual(b);
+  expect(a).toBe(c);
+});
+it("exists", () => {
+  expect(enhancer).toBeDefined();
+});
+it("repair", () => {
+  expect(
+    enhancer.repair({
+      name: "Phone",
+      enhancement: 10,
+      durability: 20,
+    })
+  ).toEqual({
+    name: "Phone",
+    enhancement: 10,
+    durability: 100,
+  });
+});
+
+it("success the enhancement increases by 1, durability is the same", () => {
+  expect(
+    enhancer.success({
+      name: "Tablet",
+      enhancement: 15,
+      durability: 50,
+    })
+  ).toEqual({
+    name: "Tablet",
+    enhancement: 16,
+    durability: 50,
+  });
+});
+
+it("fails if item is less than 15, durability is decreased by 5, enhancement is the same", () => {
+  expect(
+    enhancer.fail({
+      name: "Television",
+      enhancement: 13,
+      durability: 10,
+    })
+  ).toEqual({
+    name: "Television",
+    enhancement: 13,
+    durability: 5,
+  });
+});
+
+it("fails if item is more than or equal to 15, durability is decreased by 10, enhancement is the same", () => {
+  expect(
+    enhancer.fail({
+      name: "Stove",
+      enhancement: 15,
+      durability: 30,
+    })
+  ).toEqual({
+    name: "Stove",
+    enhancement: 15,
+    durability: 20,
+  });
+});
+
+it("fails if item is more than 16, enhancement is decreased by one, durability is the same", () => {
+  expect(
+    enhancer.fail({
+      name: "Laptop",
+      enhancement: 17,
+      durability: 35,
+    })
+  ).toEqual({
+    name: "Laptop",
+    enhancement: 16,
+    durability: 35,
+  });
+});
+
+it("gets the item", () => {
+  expect(enhancer.get).toBeDefined();
+});
